8b924165b9d5ebc8dbb286c2340340d0f3ba569f
starasov <starasov@synergy.ru>
2021-01-14 16:00:09 +0300

    menu file added

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({});
@@ -15,4 +16,49 @@ app.on('ready', () => {
     protocol: 'file:',
     slashes: true,
   }));
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // Insert menu
+  Menu.setApplicationMenu(mainMenu);
 });
+
+// Handle create add window
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 200,
+    height: 300,
+    title: 'add smthn',
+  });
+
+  addWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'addWindow.html'),
+    protocol: 'file:',
+    slashes: true,
+  }));
+}
+
+// Create menu template
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Add Item',
+        click() {
+          createAddWindow();
+        },
+      },
+      {
+        label: 'Clear Items',
+      },
+      {
+        label: 'Quit',
+        accelerator: process.platform == 'darwin' ? 'Command+Q'
+          : 'Ctrl+Q',
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  },
+];
